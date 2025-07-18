772fb57549e4a6981320fae41036f174f100978e
Aditya Roy <mr.adityaroy101@gmail.com>
2025-07-18 12:09:04 +0530

    Market Data Ingestion Service Init

---
+++ Market-Data-Ingestion-Service/src/main/java/com/md/marketdataingestionservice/MarketDataIngestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.md.marketdataingestionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataIngestionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketDataIngestionServiceApplication.class, args);
+    }
+
+}
---
+++ Market-Data-Ingestion-Service/src/test/java/com/md/marketdataingestionservice/MarketDataIngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.md.marketdataingestionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataIngestionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
